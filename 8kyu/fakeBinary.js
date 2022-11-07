b3313c8a9df04307747c2769b488f0f0847dabe1
Albazc <albazcano@gmail.com>
2022-11-07 18:56:29 +0100

    add convert num into string, viceversa and multiply

--- 8kyu/fakeBinary.js
+++ 8kyu/fakeBinary.js
@@ -1,4 +1,19 @@
-/* Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+/* 
+
+##Description
+
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 
 Note: input will never be an empty string */
 
+function fakeBin(str){
+    let arr = []
+    str.split('').forEach(numStr=> {
+      if(+numStr >=5) {
+        arr.push(1)
+      } else{
+        arr.push(0)
+      }
+    })
+    return arr.join('')
+  } 
