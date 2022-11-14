b29fe34c8dbec7c09cc38d24b37113e84fbdf9a8
Albazc <albazcano@gmail.com>
2022-11-15 00:24:51 +0100

    add 7kyu folder and largest-elements kata

---
+++ 7kyu/largest-elements.js
@@ -0,0 +1,18 @@
+/* -- Description --
+
+Write a program that outputs the top n elements from a list.
+
+Example:
+
+largest(2, [7,6,5,4,3,2,1])
+// => [6,7]
+
+*/
+
+function largest(n,xs){
+    return xs.sort(function(a, b) {
+      return b - a;
+    }).filter(function(x, index){
+      return index < n;
+    }).reverse();
+}
